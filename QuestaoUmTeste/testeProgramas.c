ebfa0dd9a8a721f89d56207005003b41f0c06b2d
sthefany <sthefanymouragodinho@gmail.com>
2025-09-14 22:34:13 -0300

    subindo nada

--- QuestaoUmTeste/testeProgramas.c
+++ QuestaoUmTeste/testeProgramas.c
@@ -3,6 +3,57 @@
 #include<string.h>
 #include "programas.h"
 
+
+/*=======QUestoes Sthefany==========*/
+
+// ix) Mostrar todos os programas de uma determinada stream que acontecem em um determinado dia e horário.  -IN PROGRESS
+/*
+1. Entrar no menu de Stream
+2. Informar a frequencia desejada
+3. Informar o horario desejado
+4. Percorrer todas as categorias
+    4.1. Percorrer todos os programas
+        4.1.1. Verificar se tem a frequencia informada e o hoario de inicio informado
+            4.1.1.1. Mostrar o programa
+
+*/
+
+
+// xi) Mostrar todos os programas de um determinado dia da semana de uma determinada categoria de uma stream. -IN PROGRESS
+// 1. Entrar no menu de Stream
+// 2. Informar a periodicaidade desejada
+// 3. Informa categoria desejada
+// 4. Bucas a categoria especifica
+//     4.1. Percorrer todos os programas da Categorias
+//         4.1.1. Verificar se o programa tem a periodicidade desejada
+
+
+
+// xii) Mostrar todos os apresentadores de uma determinada stream. ok
+// 1. Entrar no menu Stream
+// 2. Usar o nome da Stream
+// 5. Percorrer a lista de apresentadores
+//     5.1. Verificar se o apresentador trabalha na stream
+//         5.1.1. Mostrar o apresentador
+
+
+// xiii)Mostrar todos os apresentadores de um determinada categoria independente da stream que o mesmo trabalha. ok
+// 1. Informar a categoria desejada
+// 2. Percorrer Apresentadores
+//     2.1. Verificar se o apresentador trabalha na categoria informada
+//         2.1.1. Mostrar o apresentador
+
+
+// xv) Permita remover um program de uma determinada categoria de uma stream. ok
+// Já feito
+
+// xvii)Permita alterar a stream que um apresentador trabalha atualmente. Lembre-se que não pode haver programa naquela stream apresentado pelo apresentador ( programa removido ou alterado o
+// apresentador).
+// Já feito  
+
+
+
+
 // teste de alocar
 
             // case 1:
